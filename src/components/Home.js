b35f2d04710dc373c0da64a5dfa6aec006ca3891
cMcVay <connor.mcvay@yahoo.com>
2022-04-08 00:09:22 -0400

    added teams list

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,11 @@ function Home() {
             <h1>Home</h1>
             <nav>
                 <Link to='/Team/0'>Frozen Acorns</Link>
-                <Link to='/Team/Mittens'>Mittens</Link>
-                <Link to='/Team/Mittens'>Mittens</Link>
-                <Link to='/Team/Mittens'>Mittens</Link>
-                <Link to='/Team/Mittens'>Mittens</Link>
-                <Link to='/Team/Mittens'>Mittens</Link>
+                <Link to='/Team/1'>Lunar Doodles</Link>
+                <Link to='/Team/2'>Magic Manatees</Link>
+                <Link to='/Team/3'>Mighty Megabytes</Link>
+                <Link to='/Team/4'>Mindful Freaks</Link>
+                <Link to='/Team/5'>Slim Aliens</Link>
             </nav>
         </div>
         
