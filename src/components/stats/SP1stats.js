febfd0940371993898526725c0723e8143faf42d
cMcVay <connor.mcvay@yahoo.com>
2022-04-16 20:12:57 -0400

    manatees wk 1

--- src/components/stats/SP1stats.js
+++ src/components/stats/SP1stats.js
@@ -10,8 +10,8 @@ let rawStatsSP1 = [
     {id:"84", game:"5 IP, 3 H, 0 ER, 2 BB, 7 K", points:"0.74", OP:"7", day:"1"},
     //Manatees
     {id:"68", game:"3-4, 1 2B, 1 HR, 3 RBI", points:"1.2", OP:"5", day:"7"},
+    {id:"39", game:"2-4, 1 HR, 3 RBI, 1 BB", points:"1.1", OP:"4", day:"8"},
     {id:"75", game:"2-4, 1 2B, 2 RBI, 1 BB", points:"0.9", OP:"3", day:"4"},
-    {id:"39", game:"2-4, 1 2B, 1 BB", points:"0.7", OP:"2", day:"2"},
     {id:"44", game:"2-3, 1 HR, 5 RBI, 1 BB", points:"1.3", OP:"5", day:"1"},
     {id:"56", game:"1-2, 1 HR, 3 RBI, 2 BB", points:"1.1", OP:"4", day:"2"},
     {id:"91", game:"2-5, 2 HR, 2 RBI", points:"1", OP:"4", day:"4"},
@@ -35,7 +35,7 @@ let rawStatsSP1 = [
     {id:"52", game:"2-4, 1 HR, 1 RBI", points:"0.7", OP:"2", day:"6"},
     {id:"79", game:"7 IP, 3 H, 1 ER, 0 BB, 5 K", points:"0.771428571428571", OP:"7", day:"3"},
     {id:"16", game:"7 IP, 3 H, 1 ER, 4 BB, 5 K", points:"0.714285714285714", OP:"7", day:"2"},
-    //megabytes
+    //megabytes -needs update
     {id:"62", game:"3-5, 1 2B, 1 HR, 3 RBI, 1 BB", points:"1.4", OP:"5", day:"6"},
     {id:"81", game:"3-5, 1 2B, 1 RBI", points:"0.8", OP:"3", day:"2"},
     {id:"21", game:"2-4, 1 2B, 1 BB", points:"0.7", OP:"2", day:"6"},
@@ -44,7 +44,7 @@ let rawStatsSP1 = [
     {id:"2", game:"2-4, 1 HR, 1 RBI, 1 BB", points:"0.9", OP:"3", day:"7"},
     {id:"69", game:"1 IP, 0 H, 0 ER, 0 BB, 2 K", points:"0.8", OP:"8", day:"2"},
     {id:"57", game:"1 IP, 0 H, 0 ER, 0 BB, 2 K", points:"0.8", OP:"8", day:"4"}, 
-    //acorns
+    //acorns -needs update
     {id:"78", game:"3-5, 2 2B, 1 HR, 5 RBI", points:"1.5", OP:"5", day:"7"},
     {id:"41", game:"4-4, 3 2B, 3 RBI", points:"1.4", OP:"5", day:"3"},
     {id:"77", game:"2-3, 1 HR, 2 RBI, 1 BB", points:"1", OP:"4", day:"2"},
